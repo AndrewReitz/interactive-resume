5616e1a3366bacc4e9327cac3040d328984a0441
Andrew Reitz <andrew@andrewreitz.com>
2013-12-08 15:05:23 -0600

    created initial build ready for release

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         watch: {
             livereload: {
                 files: ['src/index.html'],
@@ -40,18 +41,76 @@ module.exports = function (grunt) {
             dev: {
                 path: 'http://127.0.0.1:8888',
                 app: 'google-chrome'
+            },
+            release: {
+                path: 'dist/index.html',
+                app: 'google-chrome'
             }
         },
         htmllint: {
             all: ["src/index.html"]
+        },
+        smoosher: {
+            all: {
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        clean: {
+            build: ["dist"]
+        },
+        bumpup: {
+            files: ['package.json', 'bower.json']
+        },
+        copy: {
+            release: {
+                files: [
+                    {src: ['src/lib/reveal.js/lib/js/classList.js'], dest: 'dist/lib/reveal.js/lib/js/classList.js'},
+                    {src: ['src/lib/reveal.js/plugin/highlight/highlight.js'], dest: 'dist/lib/reveal.js/plugin/highlight/highlight.js'},
+                    {src: ['src/lib/reveal.js/css/print/paper.css'], dest: 'dist/lib/reveal.js/css/print/paper.css'},
+                    {src: ['src/lib/reveal.js/css/print/pdf.css'], dest: 'dist/lib/reveal.js/css/print/pdf.css'}
+                ]
+            }
+        },
+        html_minify: {
+            options: {
+            },
+            release: {
+                files: [
+                    {
+                        src: ['dist/index.html'],
+                        dest: 'dist/index.html'
+                    }
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-html');
+    grunt.loadNpmTasks('grunt-html-smoosher');
+    grunt.loadNpmTasks('grunt-bumpup');
+    grunt.loadNpmTasks('grunt-html-minify');
+
 
     // Default task
     grunt.registerTask('default', ['help']);
@@ -62,7 +121,19 @@ module.exports = function (grunt) {
         console.log("  connect:\tStart a local server for testing on port 8888");
         console.log("  dev:\tRuns all dev commands, if your are developing you want this");
         console.log("  open:dev\tOpens Google Chrome to the index page on port 8888");
+        console.log("  open:release\tOpens the release html file");
+        console.log("  build\tCreates the release file in the dist folder");
     });
 
     grunt.registerTask('dev', 'Runs all dev commands, if your are developing you want this', 'concurrent:target');
+
+    grunt.registerTask('build', 'Creates the release file in the dist folder',
+        [
+            'copy:release',
+            'smoosher',
+            'imagemin',
+            'html_minify',
+            'bumpup'
+        ]
+    );
 };
